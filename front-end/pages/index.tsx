901250918fbaee5e915abe1e5710a380023bb382
oliverLooney <oliverjlooney@gmail.com>
2023-05-14 18:30:38 +0100

    front end graph implementation

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import {FplMiniLeagueAPIResponse} from "@/utils/types";
 import {useEffect, useState} from "react";
 import axios from "axios";
-import CurrentLeagueTable from "@/components/CurrentLeagueTable";
+import CurrentLeagueOutput from "@/components/CurrentLeagueOutput";
 import LeagueHistory from "@/components/LeagueHistory";
 import DetailedHistoryTable from "@/components/DetailedLeagueHistory";
 import LeaderBoard from "@/components/LeaderBoard";
